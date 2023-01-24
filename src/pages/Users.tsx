8016974c615465b7d3c90ed3ea02523b4b8b77c2
Anastasiya Sachko <baron-nastya2009@yandex.ru>
2023-01-24 17:52:39 +0300

    feat: add loading spinner

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -4,6 +4,7 @@ import { Navigate } from 'react-router-dom';
 import { getUsers } from 'ts/api';
 import { User } from 'ts/interfaces';
 
+import Loading from '../components/Loading';
 import UsersTable from '../components/UsersTable';
 
 interface UsersProps {
@@ -12,14 +13,17 @@ interface UsersProps {
 }
 
 function Users({ isLoggedIn, token }: UsersProps) {
+  const [isDataLoading, setDataLoading] = useState(false);
   const [users, setUsers] = useState<User[] | null>(null);
 
   useEffect(() => {
     const accessToken = localStorage.getItem('accessUserToken');
     (async () => {
       if (accessToken) {
+        setDataLoading(true);
         const data = await getUsers(accessToken);
         setUsers(data);
+        setDataLoading(false);
       }
     })();
   }, [token]);
@@ -27,7 +31,7 @@ function Users({ isLoggedIn, token }: UsersProps) {
   if (!isLoggedIn) {
     return <Navigate to="/auth-app" />;
   }
-  return <UsersTable users={users} />;
+  return isDataLoading ? <Loading /> : <UsersTable users={users} />;
 }
 
 export default memo(Users);
